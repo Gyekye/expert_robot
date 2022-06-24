f05a45703e0ca38827b26f9635a3790d86cd47de
Gyekye <mrgyekye740@gmail.com>
2022-06-24 03:17:01 +0000

    :zap: Setup project

---
+++ android/app/src/main/kotlin/com/example/expert_robot/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.expert_robot
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/core/data/api_keys.dart
@@ -0,0 +1,10 @@
+/// Contains API keys
+
+class ApiKeys {
+  /// API KEY
+  static String openWeather = '7776858cd4msh7a6714a4e264267p14b535jsn528327da64a7';
+  /// API HOST
+  static String host = 'community-open-weather-map.p.rapidapi.com';
+  /// API ENDPOINT
+  static String url = 'https://community-open-weather-map.p.rapidapi.com/weather';
+}
