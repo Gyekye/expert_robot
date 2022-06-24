e1530499d2d52d42a439417195140447b856fac3
Gyekye <mrgyekye740@gmail.com>
2022-06-24 23:14:21 +0000

    :art: Add presentation layer

--- lib/features/home/presentation/pages/home_page.dart
+++ lib/features/home/presentation/pages/home_page.dart
@@ -20,13 +20,98 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: AppBar(
         elevation: 0,
-        centerTitle: true,
-        title: const Text('London, GB'),
+        title: const Text('Accra, GH'),
         actions: [
-          IconButton(onPressed: ()async {
-            await bloc.retrieveForecast('Accra');
-          }, icon: const Icon(Icons.search_outlined))
+          IconButton(
+              onPressed: () async {
+                return await showModalBottomSheet(
+                    shape: const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.only(
+                            topRight: Radius.circular(20),
+                            topLeft: Radius.circular(20))),
+                    context: context,
+                    builder: (context) => Container(
+                          padding: const EdgeInsets.all(20),
+                          width: double.infinity,
+                          height: MediaQuery.of(context).size.height * 0.3,
+                        ));
+              },
+              icon: const Icon(Icons.travel_explore_outlined))
         ],
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // City
+              Padding(
+                padding: const EdgeInsets.only(top: 20, bottom: 10),
+                child: Text(
+                  'Accra',
+                  style: Theme.of(context)
+                      .textTheme
+                      .headline6!
+                      .apply(fontWeightDelta: 2),
+                ),
+              ),
+              // Current date
+              Padding(
+                padding: const EdgeInsets.only(bottom: 20),
+                child: Text(
+                  'Wednesday - 24 June',
+                  style: Theme.of(context).textTheme.headline5,
+                ),
+              ),
+              // Temperature
+              Text(
+                '14*',
+                style: Theme.of(context).textTheme.headline1!.apply(
+                    fontSizeFactor: 1.8,
+                    fontWeightDelta: 6,
+                    color: Colors.black),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 30),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Its the sun time',
+                      style: Theme.of(context).textTheme.headline6,
+                    ),
+                    Text(
+                      '15* - 17*',
+                      style: Theme.of(context).textTheme.headline6,
+                    ),
+                  ],
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [Text('Wind: 29kmh',style: Theme.of(context).textTheme.headline6,),
+                  Text(
+                    'Humility: 23%',
+                    style: Theme.of(context).textTheme.headline6,
+                  ),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 30),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [Text('Pressure: 1002Pa',style: Theme.of(context).textTheme.headline6,),
+                    Text(
+                      'visibility: 2000',
+                      style: Theme.of(context).textTheme.headline6,
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -15,9 +15,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Expert Robot',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.deepPurple,
       ),
       home: const HomePage()
     );
